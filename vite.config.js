7f8c7d0e88c2d5fb9b0bfe9773b0f954817ca9f9
Elayaraja G <elayaraja.g.s.140@email.com>
2026-07-02 14:13:33 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        notFound: "public/404.html",
+      },
+    },
+  },
 });
